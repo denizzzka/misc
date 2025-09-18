47f42da820bbd6e2c2a0772ddfa31a737dc76375
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-09-18 21:46:07 +0300

    implemented getTrueRandom for Windows (not tested)

--- random/source/truerandom.d
+++ random/source/truerandom.d
@@ -1,29 +1,41 @@
 ///
 module std.experimental.random.truerandom;
 
-version (D_Exceptions)
-{
-    //~ void trueRandomEx(ref ubyte[] result); // throws if no enough entropy
-}
-
-// BetterC compatible:
-
-private extern(C) int getentropy(scope void* buf, size_t buflen) @system;
+version (Posix)
+private extern(C) int getentropy(scope void* buf, size_t buflen) @system nothrow @nogc;
 
 /// Blocks and waits if no enough entropy
-void getTrueRandom(ubyte[] result) @trusted
+void getTrueRandom(scope ubyte[] result)
 {
-    version (Windows)
+    import std.exception : enforce;
+
+    version (Posix)
     {
-    }
-    else version (Posix)
-    {
-        import std.exception : enforce, ErrnoException;
+        //FIXME: https://github.com/dlang/dmd/pull/21836#issuecomment-3309075818
+        //~ import core.sys.posix.unistd : getentropy;
+        import std.exception : ErrnoException;
 
         assert(result.length <= 256);
         const status = getentropy(result.ptr, result.length);
         enforce!ErrnoException(status == 0);
     }
+    else version (Windows)
+    {
+        import core.sys.windows.bcrypt : BCryptGenRandom, BCRYPT_USE_SYSTEM_PREFERRED_RNG;
+        import core.sys.windows.windef : HMODULE, PUCHAR, ULONG;
+        import core.sys.windows.ntdef : NT_SUCCESS;
+
+        const status = BCryptGenRandom(
+            null, // hAlgorithm, null due to BCRYPT_USE_SYSTEM_PREFERRED_RNG used
+            /*cast(PUCHAR)*/ result.ptr,
+            /*cast(ULONG)*/ result.length,
+            BCRYPT_USE_SYSTEM_PREFERRED_RNG
+        );
+
+        enforce(status == STATUS_SUCCESS);
+    }
+    else
+        static assert("Unsupported platform");
 }
 
 unittest
